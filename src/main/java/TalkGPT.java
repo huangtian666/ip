6eb40b0b0461eae4dd202947322d54883c9ce184
huangtian666 <ht47275@gmail.com>
2025-01-23 23:29:52 +0800

    improved skeletal version

--- src/main/java/TalkGPT.java
+++ src/main/java/TalkGPT.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class TalkGPT {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         String name = "TalkGPT";
+        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
 
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?\n\nBye. Hope to see you again.");
+        while (true) {
+            String request = sc.nextLine();
+            if (request.toLowerCase().equals("bye")) {
+                break;
+            }
+        }
 
+        System.out.println("Goodbye! See you next time!");
     }
 }
