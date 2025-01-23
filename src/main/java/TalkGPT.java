cb8938c2190474fee3b4d37cd449e10f1d52c39e
huangtian666 <ht47275@gmail.com>
2025-01-23 22:45:02 +0800

    renamed chatbot and added initial skeleton

---
+++ src/main/java/TalkGPT.java
@@ -0,0 +1,9 @@
+public class TalkGPT {
+    public static void main(String[] args) {
+        String name = "TalkGPT";
+
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?\n\nBye. Hope to see you again.");
+
+    }
+}
